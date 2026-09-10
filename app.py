a79a5a520ef9c639b7514a688412a3cf978e75db
hakrfhc-cmyk <hakrfhc@gmail.com>
2026-09-10 21:35:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,21 @@ def home():
   return render_template("index.html")
 
 
+@app.route("/grades")
+def grades():
+  return render_template("grades.html")
+
+
+@app.route("/subjects")
+def subjects():
+  return render_template("subjects.html")
+
+
+@app.route("/quiz")
+def quiz():
+  return render_template("quiz.html")
+
+
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=10000)
+
